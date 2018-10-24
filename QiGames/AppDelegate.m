4e06b40c9e2e2debb5ff56af30e0b143dd306a90
huangxianshuai <huangxianshuai@360.cn>
2018-10-24 11:41:28 +0800

    lanchboard

--- QiGames/AppDelegate.m
+++ QiGames/AppDelegate.m
@@ -16,7 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [NSThread sleepForTimeInterval:2.0];
+    
     return YES;
 }
 
